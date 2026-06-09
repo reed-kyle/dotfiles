4aa804e606da159512658f093343f42f8411fa5a
Kyle Reed <54904309+reed-kyle@users.noreply.github.com>
2026-06-09 18:51:25 +0100

    remove go toolchain, dead config files, and stale gitignore

--- dot_config/nvim/lua/plugins/extras.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- workaround: refactoring.nvim 2.0 requires async.nvim
-  -- but LazyVim hasn't released the fix yet (LazyVim#7130)
-  -- TODO: remove once LazyVim includes this dependency
-  { "lewis6991/async.nvim" },
-}
